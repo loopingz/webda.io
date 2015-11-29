96503d92ce476605974ab79f853d18f38b901af6
Remi Cattiau <rcattiau@loopingz.com>
2015-11-28 17:00:01 -0800

    Handle environment variable for configuration

--- packages/webda/configloader.js
+++ packages/webda/configloader.js
@@ -6,13 +6,16 @@ var ConfigLoader = function() {
 	if (process.env.WEBDA_CONFIG == undefined) {
 		config = './webda-config.json';
 		if (fs.existsSync(config)) {
-			return require(config);
+			result = require(config);
 		}
 		config = '/etc/webda-config.json';
-		if (fs.existsSync(config)) {
-			return require(config);
+		if (result == undefined && fs.existsSync(config)) {
+			result = require(config);
 		}
+	} else {
+		result = require(process.env.WEBDA_CONFIG);
 	}
+	return result;
 	// Load from URL
 	console.log("Configuration can't be found");
 }

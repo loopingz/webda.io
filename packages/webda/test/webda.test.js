3d7a37bfc5f15f06d69840caf588696a349595cf
Rémi Cattiau <remi@cattiau.com>
2018-04-11 16:42:25 -0700

    Move to 0.9.1

--- packages/webda/test/webda.test.js
+++ packages/webda/test/webda.test.js
@@ -53,7 +53,7 @@ describe('Webda', function() {
   });
   describe('getVersion()', function() {
     it('current', function() {
-      assert.equal(webda.getVersion(), '0.9.0');
+      assert.equal(webda.getVersion(), '0.9.1');
     });
   });
   describe('utils', function() {

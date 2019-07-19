e02d7a682b2ee2ebbfce4eabb3d0f9a8a4831a53
Remi Cattiau <remi@cattiau.com>
2019-07-19 00:31:36 -0700

    Update to 0.11.3 version

--- packages/webda/test/webda.test.js
+++ packages/webda/test/webda.test.js
@@ -46,7 +46,7 @@ describe("Webda", function() {
   });
   describe("getVersion()", function() {
     it("current", function() {
-      assert.equal(webda.getVersion(), "0.11.2");
+      assert.equal(webda.getVersion(), "0.11.3");
     });
   });
   describe("utils", function() {

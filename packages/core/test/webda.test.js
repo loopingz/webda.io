63573dd63bd0a4094e0a22fb7d3870fb56261c4c
Remi Cattiau <remi@cattiau.com>
2019-08-21 00:18:16 -0700

    fix: use package.json variable in version unit test

--- packages/core/test/webda.test.js
+++ packages/core/test/webda.test.js
@@ -46,7 +46,7 @@ describe("Webda", function() {
   });
   describe("getVersion()", function() {
     it("current", function() {
-      assert.equal(webda.getVersion(), "0.11.3");
+      assert.equal(webda.getVersion(), require("../package.json").version);
     });
   });
   describe("utils", function() {

9689a4171164a0280ea3fa38695c97190a291569
Remi Cattiau <remi@cattiau.com>
2019-10-19 12:03:55 +0200

    fix: use 10.x for every deploy for lambda

--- packages/shell/src/deployers/lambda.ts
+++ packages/shell/src/deployers/lambda.ts
@@ -392,7 +392,7 @@ export class LambdaDeployer extends AWSDeployer {
       FunctionName: this._lambdaFunctionName,
       Handler: this._lambdaHandler,
       Role: this._lambdaRole,
-      Runtime: "nodejs8.10",
+      Runtime: "nodejs10.x",
       Timeout: this._lambdaTimeout,
       Description: "Deployed with Webda for API: " + this._restApiName
     };

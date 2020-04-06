15080cd397f947e1a739995a64ae33f3abe4e6ba
Remi Cattiau <remi@cattiau.com>
2020-04-06 01:51:29 -0700

    ci: remove dynamic.ts

--- sample-app/src/services/dynamic.ts
+++
@@ -1,9 +0,0 @@
-import { Context, Route, Service } from "@webda/core";
-
-class DynamicService extends Service {
-    @Route("/myNewRoute", ["GET"])
-    test(ctx: Context) {
-        ctx.write("Debugger Rox!");
-    }
-}
-      

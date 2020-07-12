8358e2a480a392a34286a996048471293a565caf
Remi Cattiau <remi@cattiau.com>
2020-07-12 13:22:48 -0700

    ci: wip

--- .ci/build.sh
+++ .ci/build.sh
@@ -8,6 +8,8 @@ case "$1" in
         docker run --name localstack -e SERVICES="s3,dynamodb,secretsmanager,sqs,logs,elasticsearch" -d -p 4567-4600:4567-4600 localstack/localstack
         # Wait for local stack
         echo "Waiting for LocalStack to be Ready"; while :; do docker logs localstack 2>&1 | grep Ready > /dev/null; e=$?; if [ $e -eq 0 ]; then break; fi; sleep 1; done
+        lerna run test --scope $PACKAGE
+        ;;
     *)
         # Default build
         lerna run test --scope $PACKAGE

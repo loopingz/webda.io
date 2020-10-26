faae1ac6ed18c9fe1c6b4d11d1ac3763d0e8ce77
Remi Cattiau <remi@cattiau.com>
2020-10-26 00:26:18 -0700

    ci: use older version of localstack for now

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,7 +5,7 @@ PACKAGE=$1
 case "$1" in
     "@webda/aws")
         # Run local stack
-        docker run --name localstack -e SERVICES="s3,dynamodb,secretsmanager,sqs,logs,elasticsearch" -d -p 4567-4600:4567-4600 localstack/localstack
+        docker run --name localstack -e SERVICES="s3,dynamodb,secretsmanager,sqs,logs,elasticsearch" -d -p 4567-4600:4567-4600 localstack/localstack:0.11.4
         # Wait for local stack
         echo "Waiting for LocalStack to be Ready"; while :; do docker logs localstack 2>&1 | grep Ready > /dev/null; e=$?; if [ $e -eq 0 ]; then break; fi; sleep 1; done
         lerna run test --scope $PACKAGE

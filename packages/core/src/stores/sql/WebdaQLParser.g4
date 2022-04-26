08cd39ea581f3ab5af88849cf08abf55e8d84aa7
Remi Cattiau <remi@cattiau.com>
2022-04-26 12:19:45 -0700

    feat: add WebdaQL parser

---
+++ packages/core/src/stores/sql/WebdaQLParser.g4
@@ -0,0 +1,52 @@
+grammar WebdaQLParser;
+
+import WebdaQLLexer;
+
+// Entrypoint
+webdaql: expression EOF;
+
+// Structure of operations, function invocations and expression
+expression
+    :
+    // LIKE, EXISTS and IN takes precedence over all the other binary operators
+    identifier LIKE stringLiteral #likeExpression
+    | identifier IN setExpression #inExpression
+    // Comparison operations
+    | identifier (EQUAL | NOT_EQUAL | GREATER_OR_EQUAL | LESS_OR_EQUAL | LESS | GREATER) values #binaryComparisonExpression
+    // Logic operations
+    | expression AND expression #andLogicExpression
+    | expression OR expression #orLogicExpression
+    // Subexpressions and atoms
+    | LR_BRACKET expression RR_BRACKET #subExpression
+    | atom #atomExpression
+    ;
+
+values
+    : booleanLiteral #booleanAtom
+    | integerLiteral #integerAtom
+    | stringLiteral #stringAtom
+    ;
+
+atom
+    : values # valuesAtom
+    | identifier #identifierAtom
+    ;
+
+// Identifiers
+
+identifier
+    : (IDENTIFIER | IDENTIFIER_WITH_NUMBER)
+    ;
+
+// Literals
+
+booleanLiteral: (TRUE | FALSE);
+stringLiteral: (DQUOTED_STRING_LITERAL | SQUOTED_STRING_LITERAL);
+integerLiteral: INTEGER_LITERAL;
+
+
+// Sets
+
+setExpression
+    : LR_SQ_BRACKET values ( COMMA values )* RR_SQ_BRACKET // Empty sets are not allowed
+    ;
---
+++ packages/core/src/stores/sql/query.spec.ts
@@ -0,0 +1,88 @@
+import { suite, test } from "@testdeck/mocha";
+
+import { QueryValidator } from "./query";
+import * as assert from "assert";
+
+const targets = [
+  {
+    test: {
+      attr1: "plop"
+    },
+    attr2: "OK",
+    attr3: 13,
+    attr4: "ok"
+  }
+];
+
+@suite
+class QueryTest {
+  @test
+  dev() {
+    let queryMap = {
+      "a = 1 AND b = 2 AND c = 3 AND d = 4": "a = 1 AND b = 2 AND c = 3 AND d = 4",
+      "a = 1 AND (b = 2 AND c = 3) AND d = 4": "a = 1 AND b = 2 AND c = 3 AND d = 4",
+      "a = 1 AND ((b = 2 AND c = 3) AND d = 4)": "a = 1 AND b = 2 AND c = 3 AND d = 4",
+      "a = 1 AND ((b = 2 OR c = 3) AND d = 4)": "a = 1 AND ( b = 2 OR c = 3 ) AND d = 4",
+      "a = 1 AND (b = 2) AND c = 3 AND d = 4": "a = 1 AND b = 2 AND c = 3 AND d = 4",
+      "a = 1 OR b = 2 OR c = 3 OR d = 4": "a = 1 OR b = 2 OR c = 3 OR d = 4",
+      'a = 1 AND (b = 2 OR c = 3) AND d = "plop"': 'a = 1 AND ( b = 2 OR c = 3 ) AND d = "plop"',
+      "a = 1 OR b = TRUE AND c = FALSE OR d = 'plop'": 'a = 1 OR ( b = TRUE AND c = FALSE ) OR d = "plop"',
+      "test.attr1 = 'plop'": true,
+      "test.attr1 = 'plop2'": false,
+      "test.attr1 = 'plop' AND attr2 IN ['TEST', TRUE, 3]": 'test.attr1 = "plop" AND attr2 IN ["TEST", TRUE, 3]',
+      "(test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK']) OR attr3 <= 12": null,
+      "test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK'] OR attr3 <= 12 AND attr4 = 'ok'": null,
+      "test.attr1 = 'plop' AND (attr2 IN ['TEST', 'OK'] OR attr3 <= 12) AND attr4 = 'ok'": null,
+      "test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK'] AND attr3 <= 12 AND attr4 = 'ok'": null,
+      "a = 1 OR b = 2": null,
+      "a = 1 AND b = 2 OR c = 3 AND d = 4": null,
+      "a = 1 AND b = 2 OR c = 3 AND d = 4 OR e = 5 AND f = 6": null,
+      "a = 1 AND (b = 2 OR (c = 3 AND (d = 4 OR e = 5))) AND f = 6": null,
+      "a = 1 AND b=2 OR a=1 AND b=3": "( a = 1 AND b = 2 ) OR ( a = 1 AND b = 3 )" // TODO Might want to auto-simplify to a = 1 AND b IN [2,3]
+    };
+    for (let query in queryMap) {
+      const validator = new QueryValidator(query);
+      if (typeof queryMap[query] === "string") {
+        assert.strictEqual(
+          validator.getExpression().toString(),
+          queryMap[query],
+          `Failed optimization of query: ${query} => ${validator.getExpression().toString()}`
+        );
+      } else if (typeof queryMap[query] === "boolean") {
+        assert.strictEqual(validator.eval(targets[0]), queryMap[query], `Failed query: ${query}`);
+      } else {
+        console.log("QUERY", query);
+        console.log("OPTIMIZED QUERY", validator.getExpression().toString(), "=>", validator.eval(targets[0]));
+      }
+    }
+  }
+  @test
+  simple() {
+    assert.strictEqual(new QueryValidator("test.attr1 = 'plop'").eval(targets[0]), true);
+    assert.strictEqual(new QueryValidator("test.attr1 = 'plop2'").eval(targets[0]), false);
+  }
+  @test
+  andQuery() {
+    assert.strictEqual(new QueryValidator("test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK']").eval(targets[0]), true);
+  }
+
+  @test
+  orQuery() {
+    new QueryValidator("(test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK']) OR attr3 <= 12").eval(targets[0]);
+  }
+
+  @test
+  multipleQuery() {
+    new QueryValidator("test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK'] OR attr3 <= 12 AND attr4 = 'ok'").eval(
+      targets[0]
+    );
+    new QueryValidator("test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK'] AND attr3 <= 12 AND attr4 = 'ok'").eval(
+      targets[0]
+    );
+  }
+
+  @test
+  badQuery() {
+    assert.throws(() => new QueryValidator("test lo 'plop'").eval(targets[0]));
+  }
+}

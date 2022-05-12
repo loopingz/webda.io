1f2c25e4f79e2dbe78190b334b4ab7e26a180446
Remi Cattiau <remi@cattiau.com>
2022-05-11 23:08:46 -0700

    fix: add missing files

---
+++ packages/core/src/utils/httpcontext.spec.ts
@@ -0,0 +1,14 @@
+import * as assert from "assert";
+import { suite, test } from "@testdeck/mocha";
+import { HttpContext } from "./httpcontext";
+
+@suite
+class HttpContextTest {
+  @test
+  lowerCaseHeader() {
+    let ctx = new HttpContext("test.webda.io", "GET", "/test", "http", 80, undefined, { "X-Test": "weBda" });
+    assert.strictEqual(ctx.getHeader("X-Test"), "weBda");
+    assert.strictEqual(ctx.getHeader("X-Test"), ctx.getHeader("x-test"));
+    assert.strictEqual(ctx.getPort(), "");
+  }
+}

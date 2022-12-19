8805ba657a0d574528f733e00fef1c2dae474617
Remi Cattiau <remi@cattiau.com>
2022-12-18 20:27:47 -0800

    feat: add Throttler util

---
+++ packages/core/src/utils/throttler.spec.ts
@@ -0,0 +1,37 @@
+import { suite, test } from "@testdeck/mocha";
+import * as assert from "assert";
+import { Throttler } from "./throttler";
+
+@suite
+class ThrottlerTest {
+  @test
+  async test() {
+    let resolvers: (() => void)[] = [];
+    let t = new Throttler(2);
+    t.queue(() => new Promise<void>(resolve => resolvers.push(resolve)), "P1");
+    t.queue(() => new Promise<void>(resolve => resolvers.push(resolve)), "P2");
+    t.queue(() => new Promise<void>(resolve => resolvers.push(resolve)), "P3");
+    t.queue(() => new Promise<void>(resolve => resolvers.push(resolve)), "P4");
+    t.queue(() => new Promise<void>(resolve => resolvers.push(resolve)), "P5");
+    t.queue(() => new Promise<void>((_, reject) => resolvers.push(reject)), "P6");
+    t.queue(() => new Promise<void>(resolve => resolvers.push(resolve)), "P7");
+    assert.strictEqual(t.getSize(), 7);
+    // @ts-ignore
+    resolvers.shift()();
+    assert.strictEqual(t.getInProgress().length, 2);
+    t.setConcurrency(1);
+    // @ts-ignore
+    resolvers.shift()();
+    await new Promise(resolve => setImmediate(resolve));
+    let curs = t.getInProgress();
+    assert.strictEqual(curs.length, 1, `Currents ${curs}`);
+    t.setConcurrency(3);
+    assert.strictEqual(t.getInProgress().length, 3);
+    let p = t.waitForCompletion();
+    resolvers.forEach(r => r());
+    await new Promise(resolve => setImmediate(resolve));
+    resolvers.forEach(r => r());
+    await p;
+    await t.waitForCompletion();
+  }
+}
---
+++ packages/core/src/utils/throttler.ts
@@ -0,0 +1,141 @@
+/**
+ * Represent an item within the Throttler
+ */
+class ThrottlerItem {
+  /**
+   * Once the promise is in progress
+   */
+  public promise?: Promise<any>;
+  constructor(public method: () => Promise<any>, public callbacks: ((res?: any) => void)[], public name: string) {}
+  toString() {
+    return `${this.name}: ${this.promise ? "IN-PROGRESS" : "QUEUED"}`;
+  }
+}
+
+/**
+ * Promise Throttler
+ *
+ * Allow you to queue promise and execute them concurrently
+ *
+ * Several libraries does that
+ *
+ * The queue method still gives you a simple Promise linked to the resolution
+ * of your queued item, so you can still wait for the execution of the specific
+ * item
+ */
+export class Throttler {
+  /**
+   * Current queue
+   */
+  protected _queue: ThrottlerItem[] = [];
+  /**
+   * Number of promises in-progress
+   */
+  current: number = 0;
+  /**
+   * Resolver for each call to waitForCompletion
+   */
+  protected _waiters: (() => void)[] = [];
+
+  /**
+   *
+   * @param concurrency max concurrent promise to execute
+   */
+  constructor(protected concurrency: number = 10) {}
+
+  /**
+   * Queue a new promise
+   *
+   * @param method executor that return the promise to queue
+   * @param name of the task, usefull when calling getInProgress
+   * @returns
+   */
+  queue(method: () => Promise<any>, name: string = `Promise_${this.getSize()}`): Promise<any> {
+    return new Promise<any>((...callbacks) => {
+      this._queue.push(new ThrottlerItem(method, callbacks, name));
+      this.add();
+    });
+  }
+
+  /**
+   * Set the concurrency
+   * @param concurrency newValue
+   *
+   * If decreased, it will be in effect only when current promises resolve
+   * If increased, it will have immediate effect
+   */
+  setConcurrency(concurrency: number) {
+    let oldValue = this.concurrency;
+    this.concurrency = concurrency;
+    // Ensure we launch new promise if concurrency has increased
+    while (oldValue < this.concurrency) {
+      this.add();
+      oldValue++;
+    }
+  }
+
+  /**
+   * Get inprogress items
+   * @returns
+   */
+  getInProgress(): ThrottlerItem[] {
+    return this._queue.filter(p => p.promise);
+  }
+
+  /**
+   * Get global queue size
+   * @returns
+   */
+  getSize(): number {
+    return this._queue.length;
+  }
+
+  /**
+   * Wait until every promise resolve
+   * @returns
+   */
+  async waitForCompletion(): Promise<void> {
+    if (this.current === 0) {
+      return;
+    }
+    return new Promise(resolve => {
+      this._waiters.push(resolve);
+    });
+  }
+
+  /**
+   * Internal manage the promise concurrency
+   * @returns
+   */
+  protected add() {
+    if (this.current >= this.concurrency) {
+      return;
+    }
+    const next = this._queue.find(p => !p.promise);
+    if (!next) {
+      if (this.current === 0) {
+        // Call all waiters
+        this._waiters.forEach(p => p());
+        this._waiters = [];
+      }
+      return;
+    }
+    this.current++;
+    next.promise = next
+      .method()
+      .then(res => {
+        // Call the resolve
+        next.callbacks[0](res);
+      })
+      .catch(err => {
+        // Call the reject
+        next.callbacks[1](err);
+      })
+      .finally(() => {
+        this.current--;
+        // Remove from queue
+        this._queue.splice(this._queue.indexOf(next), 1);
+        this.add();
+      });
+  }
+}

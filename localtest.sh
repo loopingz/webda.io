1196e8802f113ed44a3e968b14bb2edb7cd530b8
Remi Cattiau <remi@cattiau.com>
2023-02-19 15:13:53 -0800

    fix: mongodb5 breaking change

--- localtest.sh
+++ localtest.sh
@@ -7,7 +7,7 @@ docker run --name webda-localstack -v /tmp/localstack/:/var/lib/localstack/ -e D
 echo "Launching ElasticSearch"
 docker run --name webda-elasticsearch -e cluster.name=webda -e node.name=es1 -e discovery.type=single-node -e xpack.security.enabled=false -p 9200:9200 elasticsearch:8.1.3
 echo "Launching MongoDB"
-docker run --name webda-mongo -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=webda.io --name webda-mongo -p 37017:27017
+docker run --name webda-mongo -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=webda.io --name webda-mongo -p 37017:27017 -d mongo
 
 echo "Launching Postgres"
 docker run --name webda-postgres -v `pwd`/packages/postgres/test/sql:/docker-entrypoint-initdb.d -it -e POSTGRES_PASSWORD=webda.io -e POSTGRES_DB=webda.io -e POSTGRES_USER=webda.io -p 5432:5432 docker.io/library/postgres:13

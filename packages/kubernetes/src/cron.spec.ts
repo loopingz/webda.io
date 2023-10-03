7117c6b74451c2f4501ecb44cfd317848a3d53ec
Remi Cattiau <remi@cattiau.com>
2023-10-03 11:35:20 -0700

    feat: update CronReplace for kubernetes cron generation

--- packages/kubernetes/src/cron.spec.ts
+++ packages/kubernetes/src/cron.spec.ts
@@ -7,14 +7,30 @@ import { CronReplace } from "./cron";
 class CronTest extends WebdaTest {
   @test
   basic() {
-    let res = CronReplace(
-      {
-        value: ["a", "${...cron.args}"]
-      },
+    let text = `{
+      "value": ["a", "\${cron.argsArray}"],
+      "value2": ["a", "\${...cron.args}"],
+      "cmdline": "echo 'a' '\${cron.argsLine}'"
+    }`;
+    let res = JSON.parse(CronReplace(
+      text,
       { args: ["b", "c"], cron: "* * * * *", description: "Test", method: "method", serviceName: "service" },
       this.webda.getApplication(),
       {}
-    );
+    ));
+    assert.deepStrictEqual(res.value, ["a", "b", "c"]);
+    assert.deepStrictEqual(res.value2, ["a", "b", "c"]);
+    assert.strictEqual(res.cmdline, "echo 'a' 'b' 'c'");
+    assert.strictEqual(
+      CronReplace(`cmdline2:\n  - echo "a" "\${cron.argsLine}"\n`, { args: ["b", "c"], cron: "* * * * *", description: "Test", method: "method", serviceName: "service" },
+        this.webda.getApplication()), `cmdline2:\n  - echo "a" "b" "c"\n`);
+    res = CronReplace({
+      value: [
+        "a",
+        "${...cron.args}"
+      ]
+    }, { args: ["b", "c"], cron: "* * * * *", description: "Test", method: "method", serviceName: "service" },
+    this.webda.getApplication())
     assert.deepStrictEqual(res.value, ["a", "b", "c"]);
   }
 }

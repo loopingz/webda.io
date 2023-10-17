df43fb45ac2410003bb72787deba462e4297882f
Remi Cattiau <remi@cattiau.com>
2023-10-16 18:29:22 -0700

    ci: only testEmpty on FileBinary

--- packages/core/src/services/binary.spec.ts
+++ packages/core/src/services/binary.spec.ts
@@ -7,7 +7,6 @@ import { Store, User, WebContext, WebdaError } from "../index";
 import { CoreModel } from "../models/coremodel";
 import { WebdaTest } from "../test";
 import {
-  Binary,
   BinaryEvents,
   BinaryFileInfo,
   BinaryMap,
@@ -230,12 +229,6 @@ class BinaryTest<T extends BinaryService = BinaryService> extends WebdaTest {
     binary.handleBinary = () => -1;
     // @ts-ignore
     assert.throws(() => binary.checkMap(new CoreModel(), "pouf"), /Unknown mapping/);
-  }
-
-  @test
-  testEmpty() {
-    assert.strictEqual(new Binary("test", <any>{}).isEmpty(), true);
-    assert.strictEqual(new Binary("test", <any>{ test: {} }).isEmpty(), false);
   }
 
   @test

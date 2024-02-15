5be442267cd8c6c4ceeba1082f5540687f7cc07c
Remi Cattiau <remi@cattiau.com>
2024-02-14 17:57:51 -0800

    feat: separate WebdaQL module

---
+++ packages/ql/src/WebdaQLParserVisitor.ts
@@ -0,0 +1,237 @@
+// Generated from src/stores/webdaql/WebdaQLParser.g4 by ANTLR 4.9.0-SNAPSHOT
+
+import { ParseTreeVisitor } from "antlr4ts/tree/ParseTreeVisitor";
+
+import {
+  AndLogicExpressionContext,
+  AtomContext,
+  AtomExpressionContext,
+  BinaryComparisonExpressionContext,
+  BooleanAtomContext,
+  BooleanLiteralContext,
+  ContainsExpressionContext,
+  ExpressionContext,
+  IdentifierAtomContext,
+  IdentifierContext,
+  InExpressionContext,
+  IntegerAtomContext,
+  IntegerLiteralContext,
+  LikeExpressionContext,
+  LimitExpressionContext,
+  OffsetExpressionContext,
+  OrderExpressionContext,
+  OrderFieldExpressionContext,
+  OrLogicExpressionContext,
+  SetExpressionContext,
+  StringAtomContext,
+  StringLiteralContext,
+  SubExpressionContext,
+  ValuesAtomContext,
+  ValuesContext,
+  WebdaqlContext
+} from "./WebdaQLParserParser";
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by `WebdaQLParserParser`.
+ *
+ * @param <Result> The return type of the visit operation. Use `void` for
+ * operations with no return type.
+ */
+export interface WebdaQLParserVisitor<Result> extends ParseTreeVisitor<Result> {
+  /**
+   * Visit a parse tree produced by the `likeExpression`
+   * labeled alternative in `WebdaQLParserParser.expression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitLikeExpression?: (ctx: LikeExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `inExpression`
+   * labeled alternative in `WebdaQLParserParser.expression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitInExpression?: (ctx: InExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `containsExpression`
+   * labeled alternative in `WebdaQLParserParser.expression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitContainsExpression?: (ctx: ContainsExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `binaryComparisonExpression`
+   * labeled alternative in `WebdaQLParserParser.expression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitBinaryComparisonExpression?: (ctx: BinaryComparisonExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `andLogicExpression`
+   * labeled alternative in `WebdaQLParserParser.expression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitAndLogicExpression?: (ctx: AndLogicExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `orLogicExpression`
+   * labeled alternative in `WebdaQLParserParser.expression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitOrLogicExpression?: (ctx: OrLogicExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `subExpression`
+   * labeled alternative in `WebdaQLParserParser.expression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitSubExpression?: (ctx: SubExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `atomExpression`
+   * labeled alternative in `WebdaQLParserParser.expression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitAtomExpression?: (ctx: AtomExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `booleanAtom`
+   * labeled alternative in `WebdaQLParserParser.values`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitBooleanAtom?: (ctx: BooleanAtomContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `integerAtom`
+   * labeled alternative in `WebdaQLParserParser.values`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitIntegerAtom?: (ctx: IntegerAtomContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `stringAtom`
+   * labeled alternative in `WebdaQLParserParser.values`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitStringAtom?: (ctx: StringAtomContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `valuesAtom`
+   * labeled alternative in `WebdaQLParserParser.atom`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitValuesAtom?: (ctx: ValuesAtomContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `identifierAtom`
+   * labeled alternative in `WebdaQLParserParser.atom`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitIdentifierAtom?: (ctx: IdentifierAtomContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.webdaql`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitWebdaql?: (ctx: WebdaqlContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.limitExpression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitLimitExpression?: (ctx: LimitExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.offsetExpression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitOffsetExpression?: (ctx: OffsetExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.orderFieldExpression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitOrderFieldExpression?: (ctx: OrderFieldExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.orderExpression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitOrderExpression?: (ctx: OrderExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.expression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitExpression?: (ctx: ExpressionContext) => Result;
+
+  /**
+   * Visit a parse tree produced by the `values`
+   * labeled alternative in `WebdaQLParserParser.expressionexpressionexpressionexpressionexpressionexpressionexpressionexpressionvaluesvaluesvaluesatomatom`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitValues?: (ctx: ValuesContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.atom`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitAtom?: (ctx: AtomContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.identifier`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitIdentifier?: (ctx: IdentifierContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.booleanLiteral`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitBooleanLiteral?: (ctx: BooleanLiteralContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.stringLiteral`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitStringLiteral?: (ctx: StringLiteralContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.integerLiteral`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitIntegerLiteral?: (ctx: IntegerLiteralContext) => Result;
+
+  /**
+   * Visit a parse tree produced by `WebdaQLParserParser.setExpression`.
+   * @param ctx the parse tree
+   * @return the visitor result
+   */
+  visitSetExpression?: (ctx: SetExpressionContext) => Result;
+}
---
+++ packages/ql/src/query.spec.ts
@@ -0,0 +1,213 @@
+import { suite, test } from "@testdeck/mocha";
+import * as assert from "assert";
+import * as WebdaQL from "./query";
+
+const targets = [
+  {
+    test: {
+      attr1: "plop"
+    },
+    attr2: "OK",
+    attr3: 13,
+    attr4: "ok",
+    attr5: ["test", "plip"]
+  }
+];
+
+@suite
+class QueryTest {
+  @test
+  dev() {
+    let queryMap = {
+      "a = 1 AND b = 2 AND c = 3 AND d = 4": "a = 1 AND b = 2 AND c = 3 AND d = 4",
+      "a = 1 AND (b = 2 AND c = 3) AND d = 4": "a = 1 AND b = 2 AND c = 3 AND d = 4",
+      "a = 1 AND ((b = 2 AND c = 3) AND d = 4)": "a = 1 AND b = 2 AND c = 3 AND d = 4",
+      "a = 1 AND ((b = 2 OR c = 3) AND d = 4)": "a = 1 AND ( b = 2 OR c = 3 ) AND d = 4",
+      "a = 1 AND (b = 2) AND c = 3 AND d = 4": "a = 1 AND b = 2 AND c = 3 AND d = 4",
+      "a = 1 OR b = 2 OR c = 3 OR d = 4": "a = 1 OR b = 2 OR c = 3 OR d = 4",
+      'a = 1 AND (b = 2 OR c = 3) AND d = "plop"': 'a = 1 AND ( b = 2 OR c = 3 ) AND d = "plop"',
+      "a = 1 OR b = TRUE AND c = FALSE OR d = 'plop'": 'a = 1 OR ( b = TRUE AND c = FALSE ) OR d = "plop"',
+      "test.attr1 = 'plop'": true,
+      "test.attr1 = 'plop2'": false,
+      "test.attr1 = 'plop' AND attr2 IN ['TEST', TRUE, 3]": 'test.attr1 = "plop" AND attr2 IN ["TEST", TRUE, 3]',
+      "(test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK']) OR attr3 <= 12": null,
+      "test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK'] OR attr3 <= 12 AND attr4 = 'ok'": null,
+      "test.attr1 = 'plop' AND (attr2 IN ['TEST', 'OK'] OR attr3 <= 12) AND attr4 = 'ok'": null,
+      "test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK'] AND attr3 <= 12 AND attr4 = 'ok'": null,
+      "a = 1 OR b = 2": null,
+      "a = 1 AND b = 2 OR c = 3 AND d = 4": null,
+      "a = 1 AND b = 2 OR c = 3 AND d = 4 OR e = 5 AND f = 6": null,
+      "a = 1 AND (b = 2 OR (c = 3 AND (d = 4 OR e = 5))) AND f = 6": null,
+      "": true,
+      'test.attr1 LIKE "pl_p"': true,
+      'test.attr1 LIKE "pl__p"': false,
+      'attr3 LIKE "1_"': true,
+      "attr3 >= 12": true,
+      "attr3 <= 12": false,
+      "attr3 != 12": true,
+      "attr3 > 12": true,
+      "attr3 < 13": false,
+      "attr5 CONTAINS 'test'": true,
+      "attr5 CONTAINS 'test2'": false,
+      'test.attr1 LIKE "pl%"': true,
+      "a = 1 AND b=2 OR a=1 AND b=3": "( a = 1 AND b = 2 ) OR ( a = 1 AND b = 3 )", // TODO Might want to auto-simplify to a = 1 AND b IN [2,3]
+      "a = 1 ORDER BY a": null,
+      "a = 1 ORDER BY a DESC, b ASC": null,
+      "(attr3 >= 12)": true
+    };
+    for (let query in queryMap) {
+      const validator = new WebdaQL.QueryValidator(query);
+      assert.strictEqual(validator.displayTree().replace(/\s/g, ""), query.replace(/\s/g, ""));
+      if (typeof queryMap[query] === "string") {
+        assert.strictEqual(
+          validator.getExpression().toString(),
+          queryMap[query],
+          `Failed optimization of query: ${query} => ${validator.getExpression().toString()}`
+        );
+      } else if (typeof queryMap[query] === "boolean") {
+        assert.strictEqual(validator.eval(targets[0]), queryMap[query], `Failed query: ${query}`);
+      } else {
+        console.log("QUERY", query);
+        console.log("OPTIMIZED QUERY", validator.getExpression().toString(), "=>", validator.eval(targets[0]));
+      }
+    }
+  }
+
+  @test
+  prependQuery() {
+    assert.strictEqual(WebdaQL.PrependCondition("", "test='plop'"), "test='plop'");
+    assert.strictEqual(WebdaQL.PrependCondition("test='plop'", ""), "test='plop'");
+    assert.strictEqual(WebdaQL.PrependCondition("test='plip'", "test='plop'"), "(test='plop') AND (test='plip')");
+    assert.strictEqual(WebdaQL.PrependCondition("ORDER BY test", "test='plop'"), "test='plop' ORDER BY test");
+    assert.strictEqual(
+      WebdaQL.PrependCondition("ORDER BY test LIMIT 100", "test='plop'"),
+      "test='plop' ORDER BY test LIMIT 100"
+    );
+    assert.strictEqual(
+      WebdaQL.PrependCondition("ORDER BY test DESC LIMIT 100", "test='plop'"),
+      "test='plop' ORDER BY test DESC LIMIT 100"
+    );
+    assert.strictEqual(
+      WebdaQL.PrependCondition("test='plip' ORDER BY test ASC LIMIT 100", "test='plop'"),
+      "(test='plop') AND (test='plip') ORDER BY test ASC LIMIT 100"
+    );
+    assert.strictEqual(
+      WebdaQL.PrependCondition("test='plip' LIMIT 100", "test='plop'"),
+      "(test='plop') AND (test='plip') LIMIT 100"
+    );
+    assert.strictEqual(
+      WebdaQL.PrependCondition("test='plip' OFFSET 'test'", "test='plop'"),
+      "(test='plop') AND (test='plip') OFFSET 'test'"
+    );
+    assert.strictEqual(
+      WebdaQL.PrependCondition("test='ORDER BY plop' OFFSET 'test'", "test='plop'"),
+      "(test='plop') AND (test='ORDER BY plop') OFFSET 'test'"
+    );
+    assert.strictEqual(
+      WebdaQL.PrependCondition('test="ORDER BY plop" LIMIT 100', "test='plop'"),
+      "(test='plop') AND (test=\"ORDER BY plop\") LIMIT 100"
+    );
+  }
+
+  @test
+  likeRegexp() {
+    assert.deepStrictEqual(WebdaQL.ComparisonExpression.likeToRegex("test"), /test/);
+    assert.deepStrictEqual(WebdaQL.ComparisonExpression.likeToRegex("t_est"), /t.{1}est/);
+    assert.deepStrictEqual(WebdaQL.ComparisonExpression.likeToRegex("_test"), /.{1}test/);
+    assert.deepStrictEqual(WebdaQL.ComparisonExpression.likeToRegex("t\\_est"), /t_est/);
+
+    assert.deepStrictEqual(WebdaQL.ComparisonExpression.likeToRegex("t%est"), /t.*est/);
+    assert.deepStrictEqual(WebdaQL.ComparisonExpression.likeToRegex("%te?st"), /.*te\?st/);
+    assert.deepStrictEqual(WebdaQL.ComparisonExpression.likeToRegex("t\\%est"), /t%est/);
+    assert.deepStrictEqual(WebdaQL.ComparisonExpression.likeToRegex("t\\eest"), /t\\est/);
+  }
+
+  @test
+  simple() {
+    assert.strictEqual(new WebdaQL.QueryValidator("test.attr1 = 'plop'").eval(targets[0]), true);
+    assert.strictEqual(new WebdaQL.QueryValidator("test.attr1 = 'plop2'").eval(targets[0]), false);
+  }
+
+  @test
+  contains() {
+    assert.strictEqual(new WebdaQL.QueryValidator("test.attr1 CONTAINS 'plop2'").eval(targets[0]), false);
+  }
+
+  @test
+  andQuery() {
+    assert.strictEqual(
+      new WebdaQL.QueryValidator("test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK']").eval(targets[0]),
+      true
+    );
+  }
+
+  @test
+  orQuery() {
+    new WebdaQL.QueryValidator("(test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK']) OR attr3 <= 12").eval(targets[0]);
+  }
+
+  @test
+  multipleQuery() {
+    new WebdaQL.QueryValidator("test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK'] OR attr3 <= 12 AND attr4 = 'ok'").eval(
+      targets[0]
+    );
+    new WebdaQL.QueryValidator("test.attr1 = 'plop' AND attr2 IN ['TEST', 'OK'] AND attr3 <= 12 AND attr4 = 'ok'").eval(
+      targets[0]
+    );
+  }
+
+  @test
+  unsanitize() {
+    assert.strictEqual(WebdaQL.unsanitize("test.attr1 &lt; 12 AND attr2 &gt; 13"), "test.attr1 < 12 AND attr2 > 13");
+  }
+
+  @test
+  limitOffset() {
+    let val = new WebdaQL.QueryValidator('LIMIT 10 OFFSET "pl"');
+    // getQuery
+    val.getQuery();
+    assert.strictEqual(val.getLimit(), 10);
+    assert.strictEqual(val.getOffset(), "pl");
+    val = new WebdaQL.QueryValidator('OFFSET "pl2"');
+    assert.strictEqual(val.getLimit(), 1000);
+    assert.strictEqual(val.getOffset(), "pl2");
+  }
+
+  @test
+  badQuery() {
+    assert.throws(() => new WebdaQL.QueryValidator("test lo 'plop'").eval(targets[0]));
+  }
+
+  @test
+  setters() {
+    let target: any = {};
+    new WebdaQL.SetterValidator('i = 10 AND j = "12"').eval(target);
+    assert.strictEqual(target.i, 10);
+    assert.strictEqual(target.j, "12");
+    target = {};
+    new WebdaQL.SetterValidator("").eval(target);
+    assert.strictEqual(Object.keys(target).length, 0);
+    new WebdaQL.SetterValidator('test.i = 10 AND j.k.l = "12"').eval(target);
+    assert.strictEqual(target.test.i, 10);
+    assert.strictEqual(target.j.k.l, "12");
+    target = {};
+    new WebdaQL.SetterValidator("test.__proto__.test = 10").eval(target);
+    assert.strictEqual(target.__proto__.test, undefined);
+    assert.throws(() => new WebdaQL.SetterValidator('i = 10 OR j = "12"').eval(target), SyntaxError);
+    assert.throws(() => new WebdaQL.SetterValidator('i > 10 AND j = "12"').eval(target), SyntaxError);
+  }
+
+  @test
+  partialValidator() {
+    let validator = new WebdaQL.PartialValidator("attr1 = 'plop' AND attr2 = 'ok'");
+    assert.ok(validator.eval({ attr1: "plop" }));
+    assert.ok(validator.wasPartialMatch());
+    assert.ok(!validator.eval({ attr1: "plop" }, false));
+    assert.ok(!validator.eval({ attr1: "plop2" }));
+    assert.ok(
+      new WebdaQL.PartialValidator(
+        "attr1 = 'plop' AND attr2 LIKE '?ok' AND attr3 IN ['test'] AND attr4 CONTAINS 'plop'"
+      ).eval({ attr1: "plop" })
+    );
+  }
+}
